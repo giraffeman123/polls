bb40c8408d228e8b6766a066890173134538e9a0
EC2 Default User <ec2-user@ip-172-31-40-116.us-east-2.compute.internal>
2019-06-11 15:49:43 +0000

    First Commit

---
+++ webapp/polls/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('login/', views.log_in, name='login'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('create/', views.QuestionCreateView.as_view() , name = "create"),
+    #path('render/', views.index_render, name='index_render'),
+    #path('specifics/<int:question_id>/', views.detail, name='detail'),
+    #path('<int:question_id>/results/', views.results, name='results'),
+    #path('<int:question_id>/vote/', views.vote, name='vote'),
+]
